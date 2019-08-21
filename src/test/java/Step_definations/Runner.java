91fbcf291aa47ca280b0164258c72d53799b202d
Insiya1295 <54269294+Insiya1295@users.noreply.github.com>
2019-08-21 13:04:53 +0530

    Update Runner.java

--- src/test/java/Step_definations/Runner.java
+++ src/test/java/Step_definations/Runner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith (Cucumber.class)
-@CucumberOptions(features="C:\\Users\\training_h2a.06.15\\Desktop\\Selenium_workplace\\Cucumber_Demo_Project\\Feature456\\mytest999.feature",glue="Step_definations",plugin= {"html:src/cucumber-report.html","json:target/cucumber-reports.json"})
+@CucumberOptions(features="C:\\Users\\training_h2a.06.15\\Desktop\\Selenium_workplace\\Cucumber_Demo_Project\\Feature456\\mytest999.feature",glue="Step_definations",plugin= {"html:target/cucumber-report.html","json:target/cucumber-reports.json"})
 public class Runner {
 
 }
